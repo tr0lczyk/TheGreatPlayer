77dbdd2a13589f2767825d3063b94f7778af52ee
tr0lczyk <matt.olczyk@gmail.com>
2018-03-07 20:14:52 +0100

    GenreAdapter created

---
+++ app/src/main/java/com/example/android/thegreatplayer/GenreAdapter.java
@@ -0,0 +1,19 @@
+package com.example.android.thegreatplayer;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Mateusz on 07.03.2018.
+ */
+
+public class GenreAdapter extends ArrayAdapter<GenreObject> {
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
